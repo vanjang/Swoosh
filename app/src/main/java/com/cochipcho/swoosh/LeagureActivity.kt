01fd9d3238c1f858b6b304cb543ad08b0b6b51f8
Myung Hoon So <joa214@hotmail.com>
2020-06-09 08:23:25 +0100

    league view created

---
+++ app/src/main/java/com/cochipcho/swoosh/LeagureActivity.kt
@@ -0,0 +1,12 @@
+package com.cochipcho.swoosh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LeagureActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_league)
+    }
+}
